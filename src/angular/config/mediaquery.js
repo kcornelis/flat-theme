c0ecca0a03c20babde2564ca7251ffc8bdf95a72
Kevin Cornelis <kevin.cornelis@outlook.com>
2015-05-27 07:08:37 +0200

    Added media query constants and touch helper

---
+++ src/angular/config/mediaquery.js
@@ -0,0 +1,6 @@
+angular.module('ft').constant('mediaquery', {
+	'desktopLG': 1200,
+	'desktop': 992,
+	'tablet': 768,
+	'mobile': 480
+});
---
+++ src/angular/tools/touch.js
@@ -0,0 +1,16 @@
+(function($, window){
+	'use strict';
+	
+	var $html = $('html'), $win = $(window);
+
+	$.support.touch                 = (
+		('ontouchstart' in window && navigator.userAgent.toLowerCase().match(/mobile|tablet/)) ||
+		(window.DocumentTouch && document instanceof window.DocumentTouch)  ||
+		(window.navigator.msPointerEnabled && window.navigator.msMaxTouchPoints > 0) || //IE 10
+		(window.navigator.pointerEnabled && window.navigator.maxTouchPoints > 0) || //IE >=11
+		false
+	);
+
+	$html.addClass($.support.touch ? 'touch' : 'no-touch');
+
+}(jQuery, window));
