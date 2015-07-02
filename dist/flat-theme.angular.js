08324d1640927d09cb5698e7ce5051ad5338949d
Kevin Cornelis <kevin.cornelis@outlook.com>
2015-07-02 06:44:39 +0200

    Version 0.0.2

--- dist/flat-theme.angular.js
+++ dist/flat-theme.angular.js
@@ -29,6 +29,19 @@
 (function() {
 	'use strict';
 
+	angular.module('ft').run(fix);
+
+	fix.$inject = [ '$rootScope', '$window' ];
+
+	function fix($rootScope, $window) {
+		$rootScope.$on('$stateChangeSuccess', function() {
+			$window.scrollTo(0, 0);
+		});
+	}
+})();
+(function() {
+	'use strict';
+
 	var directiveName = 'ftFormGroup';
 
 	angular.module('ft').directive(directiveName, formGroupDirective);
